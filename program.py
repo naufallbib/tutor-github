0e9fd5edf80efccd4252fdb029ea38f18f657f6b
naufallbib <labibnaufal035@gmail.com>
2026-09-08 14:29:26 +0700

    merubah tulisan error

--- program.py
+++ program.py
@@ -16,4 +16,4 @@ elif operasi == '/':
     hasil = angka1 / angka2
     print(f'Hasilnya adalah {hasil}')
 else:
-    print('Masukkan operasi yang benar')
+    print('ERROR: Operasi yang anda masukkan salah')
