2fd5370e5dd0149f524890f8436494dbc12db45c
naufallbib <labibnaufal035@gmail.com>
2026-09-08 13:27:38 +0700

    finishing program

--- program.py
+++ program.py
@@ -1,1 +1,19 @@
-print ("KALKULATOR")
+print ("KALKULATOR SEDERHANA")
+
+angka1 = float(input('Masukkan angka pertama = '))
+angka2 = float(input('Masukkan angka kedua = '))
+operasi = input('Masukkan operasi (+, -, x, /)= ')
+if operasi == '+':
+    hasil = angka1 + angka2
+    print(f'Hasilnya adalah {hasil}')
+elif operasi == '-':
+    hasil = angka1 - angka2
+    print(f'Hasilnya adalah {hasil}')
+elif operasi == 'x':
+    hasil = angka1 * angka2
+    print(f'Hasilnya adalah {hasil}')
+elif operasi == '/':
+    hasil = angka1 / angka2
+    print(f'Hasilnya adalah {hasil}')
+else:
+    print('Masukkan operasi yang benar')
